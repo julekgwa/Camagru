067447dd205076ce06063bec066888f187834e95
Junius P. LEKGWARA <julekgwa@julekgwa.com>
2016-10-07 01:12:28 +0200

    moved files to view

--- footer.php
+++
@@ -1,4 +0,0 @@
-<script src="js/camagru.js"></script>
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> Camagru. Developed by <a href="https://profile.intra.42.fr/users/julekgwa"> julekgwa</a></p>
-</footer>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: julekgwa
- * Date: 2016/10/05
- * Time: 3:50 PM
- */
